ab743c086ee8d465e760f4e8ae9c4b609173c619
Luciano Lorenti <lucianolorenti@gmail.com>
2021-01-05 01:20:18 +0100

    Add to numpy method

--- bin/train.py
+++ bin/train.py
@@ -4,7 +4,9 @@ from keyword_spotting.data import Dataset
 from keyword_spotting.model import get_model_2
 
 dataset = Dataset('/home/luciano/speech')
+dataset.to_numpy()
 model = get_model_2(dataset.shape, dataset.number_of_classes)
+
 train_data, val_data, test_data = dataset.get_sequences(batch_size=256)
 model.fit(train_data,
           validation_data=val_data, epochs=15)
