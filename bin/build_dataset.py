e4eb314fed36bd6defd87c852473dda065fae2b2
Maria Emilia Charnelli <maria.emilia.charnelli@gmail.com>
2021-05-23 15:58:39 +0200

    Add functionalities to create train, test and validation sets before to create the model

--- bin/build_dataset.py
+++ bin/build_dataset.py
@@ -2,7 +2,19 @@ import argparse
 from pathlib import Path
 import hashlib
 import shutil
+import argparse
 
+import numpy as np
+import tensorflow as tf
+import yaml
+from keyword_spotting.feature_extraction.utils import (
+    extract_features as keyword_extract_features,
+)
+from keyword_spotting.feature_extraction.utils import read_wav
+from keyword_spotting.model import cnn_inception2, models
+import pickle
+from sklearn.model_selection import train_test_split
+from keyword_spotting.data.utils import distribution_labels
 
 def hash_to_int(s):
     hash_object = hashlib.sha1(str.encode(s))
@@ -28,6 +40,26 @@ folders = [
     "four", "house", "nine", "sheila", "three", "two", "visual", "zero"
 ]
 # fmt: on
+
+def generate_file_list(labels, dataset_path: Path):
+    files = []
+    for l in labels[:11]:
+        files.extend(list((dataset_path / l).glob("*.wav")))
+
+    files.extend(list((dataset_path/ 'silence').glob('*.wav'))*430)
+    
+    files = sorted(files, key=lambda path: path.stem)
+    return files
+
+def save_files_names(data_path, ds, name):
+    files = []
+    for x in ds:
+        file_parts = x.parts
+        file = f'{file_parts[-2]}/{file_parts[-1]}'
+        files.append(file)
+    DS_PATH = data_path / f'{name}.pickle'
+    with open(DS_PATH, 'wb') as f:
+        pickle.dump(files, f)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Transform")
@@ -73,3 +105,15 @@ if __name__ == "__main__":
 
     folder = "silence"
     shutil.copytree(dataset / "_background_noise_", output_path / folder)
+
+    file_list = generate_file_list(output_path)
+    X_train, X_val_test = train_test_split(file_list, test_size=0.20, random_state=42)
+    X_val, X_test = train_test_split(X_val_test, test_size=0.50, random_state=42)
+
+    print(distribution_labels(selected_keywords, X_train))
+    print(distribution_labels(selected_keywords, X_test))
+    print(distribution_labels(selected_keywords, X_val))
+
+    save_files_names(output_path, X_train, 'X_train')
+    save_files_names(output_path, X_test, 'X_test')
+    save_files_names(output_path, X_test, 'X_val')
