5fc8c4e4ddf6830c7b4dc5be773dda868c8ddd4f
Maria Emilia Charnelli <maria.emilia.charnelli@gmail.com>
2021-05-23 16:11:53 +0200

    Add keywords silence and unknow to generate the train, test and validation sets with all the labels

--- bin/build_dataset.py
+++ bin/build_dataset.py
@@ -16,10 +16,10 @@ import pickle
 from sklearn.model_selection import train_test_split
 from keyword_spotting.data.utils import distribution_labels
 
+
 def hash_to_int(s):
     hash_object = hashlib.sha1(str.encode(s))
     return int(hash_object.hexdigest(), 16)
-
 
 selected_keywords = [
     "down",
@@ -41,25 +41,28 @@ folders = [
 ]
 # fmt: on
 
-def generate_file_list(labels, dataset_path: Path):
+
+def generate_file_list(dataset_path: Path, labels):
     files = []
     for l in labels[:11]:
         files.extend(list((dataset_path / l).glob("*.wav")))
 
-    files.extend(list((dataset_path/ 'silence').glob('*.wav'))*430)
-    
+    files.extend(list((dataset_path / "silence").glob("*.wav")) * 430)
+
     files = sorted(files, key=lambda path: path.stem)
     return files
 
-def save_files_names(data_path, ds, name):
+
+def save_files_names(data_path: Path, ds, name):
     files = []
     for x in ds:
         file_parts = x.parts
-        file = f'{file_parts[-2]}/{file_parts[-1]}'
+        file = f"{file_parts[-2]}/{file_parts[-1]}"
         files.append(file)
-    DS_PATH = data_path / f'{name}.pickle'
-    with open(DS_PATH, 'wb') as f:
+    DS_PATH = data_path / f"{name}.pickle"
+    with open(DS_PATH, "wb") as f:
         pickle.dump(files, f)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Transform")
@@ -106,7 +109,9 @@ if __name__ == "__main__":
     folder = "silence"
     shutil.copytree(dataset / "_background_noise_", output_path / folder)
 
-    file_list = generate_file_list(output_path)
+    selected_keywords.extend(['unknown', 'silence'])
+    
+    file_list = generate_file_list(output_path, selected_keywords)
     X_train, X_val_test = train_test_split(file_list, test_size=0.20, random_state=42)
     X_val, X_test = train_test_split(X_val_test, test_size=0.50, random_state=42)
 
@@ -114,6 +119,6 @@ if __name__ == "__main__":
     print(distribution_labels(selected_keywords, X_test))
     print(distribution_labels(selected_keywords, X_val))
 
-    save_files_names(output_path, X_train, 'X_train')
-    save_files_names(output_path, X_test, 'X_test')
-    save_files_names(output_path, X_test, 'X_val')
+    save_files_names(output_path, X_train, "X_train")
+    save_files_names(output_path, X_test, "X_test")
+    save_files_names(output_path, X_test, "X_val")
