46cb825b6f939fbac7f424232c798fe16ea46b5f
Maria Emilia Charnelli <maria.emilia.charnelli@gmail.com>
2021-05-29 18:04:33 +0200

    Add a new model and add function to generate train, test and validate data

--- bin/train_1.py
+++ bin/train_1.py
@@ -1,5 +1,7 @@
-import argparse
 
+
+import argparse
+from time import time
 from pathlib import Path
 import pickle
 import numpy as np
@@ -10,6 +12,10 @@ from keyword_spotting.feature_extraction.utils import (
 )
 from keyword_spotting.feature_extraction.utils import read_wav
 from keyword_spotting.model import cnn_inception2, models
+from tqdm.auto import tqdm 
+import logging
+logging.basicConfig()
+logger = logging.getLogger('hda')
 
 noise_files = [
     "doing_the_dishes.wav",
@@ -33,27 +39,30 @@ labels = [
     "stop",
     "go",
     "unknown",
-    "silence",    
+    "silence",
 ]
 
+
 def create_data_path(dataset_path: Path, data):
-    return [f'{dataset_path}/{x}' for x in data]
+    return [f"{dataset_path}/{x}" for x in data]
+
 
 def load_data(dataset_path: Path):
 
-    with open(dataset_path / f'X_train.pickle', 'rb') as f:
+    with open(dataset_path / f"X_train.pickle", "rb") as f:
         X_train = pickle.load(f)
     X_train = create_data_path(dataset_path, X_train)
 
-    with open(dataset_path / f'X_val.pickle', 'rb') as f:
+    with open(dataset_path / f"X_val.pickle", "rb") as f:
         X_val = pickle.load(f)
     X_val = create_data_path(dataset_path, X_val)
 
-    with open(dataset_path / f'X_test.pickle', 'rb') as f:
+    with open(dataset_path / f"X_test.pickle", "rb") as f:
         X_test = pickle.load(f)
     X_test = create_data_path(dataset_path, X_test)
-    
-    return X_train, X_val, X_test
+
+    return X_train[:500], X_val, X_test
+
 
 lables_dict = {l: i for i, l in enumerate(labels)}
 
@@ -67,28 +76,35 @@ def tf_extract_features(sample_rate, signal, label):
     )
 
 
+
+
+
 def tf_read_wav(path):
     def read_wav_(file_path):
         file_path = bytes.decode(file_path)
         label = file_path.split("/")[-2]
         fs, data = read_wav(file_path)
         return fs, np.float32(data), np.int32(lables_dict[label])
-
     return tf.numpy_function(read_wav_, [path], [tf.int64, tf.float32, tf.int32])
 
 
-def tf_windowed(data, label, left: int = 23, right: int = 8):
-    def windowed_(data, label):
-        d = np.array(
-            [data[j - left : j + right, :] for j in range(left, data.shape[0] - right)],
-            dtype=np.float32,
-        )
-        labels = np.array(
-            [label for j in range(left, data.shape[0] - right)], dtype=np.int32
-        )
-        return d, labels
+def windowed_(data, label, left: int = 23, right: int = 9):
+    d = np.array(
+        [data[j - left : j + right, :] for j in range(left, data.shape[0] - right)],
+        dtype=np.float32,
+    )
+    labels = np.array(
+        [label for j in range(left, data.shape[0] - right)], dtype=np.int32
+    )
+    return d, labels
 
-    return tf.numpy_function(windowed_, [data, label], [tf.float32, tf.int32])
+
+def tf_windowed(data, label, left: int = 23, right: int = 9):
+    return tf.numpy_function(
+        lambda data, label: windowed_(data, label, left, right),
+        [data, label],
+        [tf.float32, tf.int32],
+    )
 
 
 def tf_add_noise(sample_rate, signal, label):
@@ -98,7 +114,8 @@ def tf_add_noise(sample_rate, signal, label):
                 data_path / "_background_noise_" / np.random.choice(noise_files)
             )
             fs, data_noise = read_wav(noise_path)
-            signal = signal + data_noise[: len(signal)]
+            min_length = min(data_noise.shape[0], signal.shape[0])
+            signal[:min_length] = signal[:min_length] + data_noise[:min_length]
         return sample_rate, signal, label
 
     return tf.numpy_function(
@@ -113,13 +130,15 @@ def split_window(x, y):
 
 def shapeify(x, y):
 
-    x.set_shape([31, 12])
+    x.set_shape([32, 12])
     y.set_shape([])
 
     return x, y
 
 
 if __name__ == "__main__":
+
+   
     parser = argparse.ArgumentParser(description="Train")
     parser.add_argument("--config", type=str, required=True, help="Config file")
 
@@ -130,36 +149,87 @@ if __name__ == "__main__":
         config = yaml.load(file.read(), Loader=yaml.SafeLoader)
 
     data_path = Path(config["dataset"]["path"])
+    output_path = Path(config["output_path"])
 
     X_train, X_val, X_test = load_data(data_path)
-    print(X_train[0])
-    
+    print(len(X_train))
+    print(len(X_val))
+    print(len(X_test))
+    print('--------------------------------------------zzzzzzzzzzzzz------------')
+
     ds_train = (
-        
         tf.data.Dataset.from_tensor_slices(X_train)
-        .shuffle(buffer_size=5000)
-        .map(tf_read_wav)
+        .shuffle(buffer_size=50000)
+        .map(tf_read_wav,        num_parallel_calls=tf.data.AUTOTUNE)
+        .cache()
         .apply(tf.data.experimental.ignore_errors())
-        .map(tf_add_noise)
-        .map(tf_extract_features)
-        .map(tf_windowed)
+        .map(tf_add_noise,        num_parallel_calls=tf.data.AUTOTUNE)
+        .map(tf_extract_features,        num_parallel_calls=tf.data.AUTOTUNE)
+        .map(tf_windowed,        num_parallel_calls=tf.data.AUTOTUNE)
         .flat_map(split_window)
         .map(shapeify)
-        .prefetch(tf.data.AUTOTUNE)
+        .prefetch(5000000)
     )
+    i = 0
+    for a in tqdm(ds_train):
+        i+=1
+    print(i)    
     number_of_classes = len(labels)
-    input_shape = [a[0].shape for a in ds_train.take(1)][0]
+    #input_shape = [a[0].shape for a in ds_train.take(1)][0]
+    input_shape = [32, 12]
     params = config["model"].get("params", {})
+
+    print('Aaaaaaaaaa')
+    print('SXSCSCDC----------dSDSDSDSSD--------1211212221112121')
+    print(f'len {ds_train }')
+    print(f'data {input_shape[0]}')
+    # 32 * 11 total number of features
+    print(f'input shape {input_shape}')
+    print('---------------BBBBBBBB------------------------------<<<<<<<<<')
     model = models[config["model"]["name"]](input_shape, number_of_classes, **params)
     model.summary()
     epochs = config["train"]["epochs"]
 
     model_path = Path(config["model"]["path"]).resolve()
 
+
+    batch_size = config["train"]["batch_size"]
     # early_stopping = tf.keras.callbacks.EarlyStopping(patience=10)
     # check_point = tf.keras.callbacks.ModelCheckpoint(model_filename)
-    history = model.fit(ds_train.batch(512), epochs=epochs)
 
     # model = cnn_inception2((input_shape[0], input_shape[1]), 10)
     # model.summary()
     # model.fit(ds, epochs=5)
+
+    ds_val = (
+        tf.data.Dataset.from_tensor_slices(X_val)
+        .map(tf_read_wav)
+        
+        .apply(tf.data.experimental.ignore_errors())
+        .prefetch(tf.data.AUTOTUNE)
+        .map(tf_extract_features)
+        .map(tf_windowed)
+        .flat_map(split_window)
+        .map(shapeify)
+        .cache()
+        .prefetch(tf.data.AUTOTUNE)
+    )
+
+    start = time()
+    history = model.fit(
+        ds_train.batch(batch_size), validation_data=ds_val.batch(batch_size), epochs=epochs
+    )
+    total_time=time()-start
+
+    results = []
+    for audio_file in tqdm(X_test):
+        label = Path(audio_file).resolve().parts[-2]
+        label = lables_dict[label]
+        sample_rate, signal = read_wav(audio_file)
+        data = keyword_extract_features(sample_rate, signal)
+        data, labels = windowed_(data, label)
+        predicted = model.predict(data)
+        results.append((audio_file, label, predicted))
+
+    with open(output_path, 'wb') as file:
+        pickle.dump((config, total_time, results), file)
--- keyword_spotting/model.py
+++ keyword_spotting/model.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from kapre.composed import get_melspectrogram_layer
 from tcn import TCN
 from tensorflow.keras import Model
 from tensorflow.python.keras import backend as K
@@ -9,11 +8,12 @@ from tensorflow.python.keras.layers import (GRU, Activation, Add,
                                             Concatenate, Conv2D, Dense, Dot,
                                             Dropout, Flatten, Input, Lambda,
                                             LayerNormalization, Reshape,
-                                            Softmax, SpatialDropout2D)
+                                            Softmax, SpatialDropout2D, 
+                                            Embedding, Layer, MultiHeadAttention)
 from tensorflow.python.ops import math_ops
-
-from keyword_spotting.data import SAMPLE_RATE
-
+import numpy as np
+from tensorflow import keras
+import tensorflow_addons as tfa
 
 def ExpandDimension():
     return Lambda(lambda x: K.expand_dims(x))
@@ -409,6 +409,103 @@ def cnn_trad_fpool3(input_shape, number_of_classes):
                   loss='sparse_categorical_crossentropy')
     return model
 
+class PatchEncoder(Layer):
+    def __init__(self, num_patches, projection_dim):
+        super(PatchEncoder, self).__init__()
+        self.num_patches = num_patches
+        self.projection = Dense(units=projection_dim)
+        self.position_embedding = Embedding(
+            input_dim=num_patches, output_dim=projection_dim
+        )
+
+    def call(self, patch):
+        positions = tf.range(start=0, limit=self.num_patches, delta=1)
+        encoded = self.projection(patch) + self.position_embedding(positions)
+        return encoded
+
+class Patches(Layer):
+    def __init__(self, patch_size):
+        super(Patches, self).__init__()
+        self.patch_size = patch_size
+
+    def call(self, images):
+        batch_size = tf.shape(images)[0]
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size[0], self.patch_size[1], 1],
+            strides=[1, self.patch_size[0], self.patch_size[1], 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+        patch_dims = patches.shape[-1]
+        patches = tf.reshape(patches, [batch_size, -1, patch_dims])
+        return patches
+
+def mlp(x, hidden_units, dropout_rate):
+    for units in hidden_units:
+        x = Dense(units, activation=tf.nn.gelu)(x)
+        x = Dropout(dropout_rate)(x)
+    return x
+
+def cnn_visiontransformer(input_shape, number_of_classes):
+    #implements the Vision Transformer (ViT) model by Alexey Dosovitskiy et al. 
+    # for image classification, and demonstrates it on the CIFAR-100 dataset.
+   
+    learning_rate = 0.001
+    weight_decay = 0.0001
+    batch_size = 256
+    num_epochs = 100
+    patch_size = (4,6)  # Size of the patches to be extract from the input images
+    num_patches = int((input_shape[0] / patch_size[0])*(input_shape[1] / patch_size[1])) 
+    projection_dim = 64
+    num_heads = 3
+    transformer_units = [
+        projection_dim * 2,
+        projection_dim,
+    ]  # Size of the transformer layers
+    transformer_layers = 4
+    mlp_head_units = [256, 128]  # Size of the dense layers of the final classifier
+
+    input = Input(shape=input_shape)
+    x = input
+    x = ExpandDimension()(x)
+    # Create patches.
+    patches = Patches(patch_size)(x)
+    # Encode patches.
+    encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
+
+    # Create multiple layers of the Transformer block.
+    for _ in range(transformer_layers):
+        # Layer normalization 1.
+        x1 = LayerNormalization(epsilon=1e-6)(encoded_patches)
+        # Create a multi-head attention layer.
+        attention_output = MultiHeadAttention(
+            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+        )(x1, x1)
+        # Skip connection 1.
+        x2 = Add()([attention_output, encoded_patches])
+        # Layer normalization 2.
+        x3 = LayerNormalization(epsilon=1e-6)(x2)
+        # MLP.
+        x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
+        # Skip connection 2.
+        encoded_patches = Add()([x3, x2])
+
+    # Create a [batch_size, projection_dim] tensor.
+    representation = LayerNormalization(epsilon=1e-6)(encoded_patches)
+    representation = Flatten()(representation)
+    representation = Dropout(0.5)(representation)
+    # Add MLP.
+    features = mlp(representation, hidden_units=mlp_head_units, dropout_rate=0.5)
+    # Classify outputs.
+
+    x = Dense(number_of_classes, activation='softmax')(features)
+    model = Model(inputs=[input], outputs=[x])
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+    model.compile(optimizer=optimizer,
+                  metrics=['accuracy'],
+                  loss='sparse_categorical_crossentropy')
+    return model
 
 models = {
     'cnn_trad_fpool3': cnn_trad_fpool3,
@@ -417,5 +514,6 @@ models = {
     'cnn_residual': cnn_residual,
     'cnn_inception': cnn_inception,
     'cnn_residual2': cnn_residual2,
-    'cnn_inception2': cnn_inception2
+    'cnn_inception2': cnn_inception2,
+    'cnn_visiontransformer':cnn_visiontransformer,
 }
